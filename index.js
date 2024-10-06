c42188c1a5117b0f9cd4a0a206e0122cdc0fa385
VedSur <vedantsur@gmail.com>
2024-10-06 10:26:35 +0530

    Fixing Blogs

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let blogs = document.getElementById("blogs");
 let no_blogs = true;
+let blogs_path = "./blogs"
 blogs.textContent = "No blogs at the moment";
 
-function addBlog(name, link) {
+function addBlog(name, relative_path, relative=true) {
     if(no_blogs) {
         no_blogs = false;
         blogs.textContent = "";
     }
+    let path = relative ? `${blogs_path}/${relative_path}` : relative_path;
     let blog = document.createElement("li");
     let link_element = document.createElement("a");
     link_element.text = name;
-    link_element.href = link;
+    link_element.href = path;
     blog.appendChild(link_element);
     blogs.appendChild(blog);
 }
 
-addBlog("Website Making", "./Website_making/index.html")
+addBlog("Website Making", "website_making")
